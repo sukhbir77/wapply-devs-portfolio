d12fc824b125f49ba1ce9de01c73a74c0549de75
Sukhbir Singh <sukhbirbrar83@gmail.com>
2021-01-20 18:03:39 -0800

    This is second time fixing the empty React files

--- src/template/archive.js
+++ src/template/archive.js
@@ -1,1 +1,7 @@
-//Helloo
+import React from "react"
+
+function Cube(){
+    return <h6> hello</h6>
+}
+
+export default Cube;
--- src/template/category-post.js
+++ src/template/category-post.js
@@ -1,1 +1,7 @@
-//Hello World
+import React from "react"
+
+function Cube(){
+    return <h6> hello</h6>
+}
+
+export default Cube;
